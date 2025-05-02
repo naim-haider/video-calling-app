c9a5952027ceb4d043eb3e0504f51ccaf629ef9e
naim-haider <naimhaider76@gmail.com>
2025-05-03 00:10:53 +0530

    added backend endpoint in fronted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://video-calling-app-nine.vercel.app/");
 
 const App = () => {
   const localVideoRef = useRef(null);
